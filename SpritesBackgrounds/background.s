1942e256e25b84b4f1ab2a841e0ca3b1abe0e3a3
print-Eruki <palejorniel@gmail.com>
2024-02-20 23:18:11 -0400

    Fixed the bonus, loading chars into the background and using loops

--- SpritesBackgrounds/background.s
+++ SpritesBackgrounds/background.s
@@ -1,27 +1,44 @@
 ;Ale J. Pagan Andujar
 ;CIIC4082 - Computer Architecture 2
 
-.include "constants.inc"
-.include "header.inc"
+PPUCTRL   = $2000
+PPUMASK   = $2001
+PPUSTATUS = $2002
+PPUSCROLL = $2005
+PPUADDR   = $2006
+PPUDATA   = $2007
+OAMADDR   = $2003
+OAMDMA    = $4014
+
+.segment "HEADER"
+.byte $4e, $45, $53, $1a ; Magic string that always begins an iNES header
+.byte $02        ; Number of 16KB PRG-ROM banks
+.byte $01        ; Number of 8KB CHR-ROM banks
+.byte %00000000  ; Horizontal mirroring, no save RAM, no mapper
+.byte %00000000  ; No special-case flags set, no mapper
+.byte $00        ; No PRG-RAM present
+.byte $00        ; NTSC format
+
+.segment "STARTUP"
 
 .segment "CODE"
-.proc irq_handler
-  RTI
-.endproc
 
-.proc nmi_handler
-  LDA #$00
-  STA OAMADDR
-  LDA #$02
-  STA OAMDMA
+
+irq_handler:
+  RTI
+
+nmi_handler:
+  ; LDA #$00
+  ; STA OAMADDR
+  ; LDA #$02
+  ; STA OAMDMA
   LDA #$00
   STA $2005
   STA $2005
   RTI
-.endproc
 
-.export reset_handler
-.proc reset_handler
+
+reset_handler:
   sei		; disable IRQs
   cld		; disable decimal mode
   ldx #$40
@@ -41,10 +58,8 @@ clear_oam:
   INX
   INX
   BNE clear_oam
-.endproc
 
-.export main
-.proc main
+main:
   ; write a palette
   LDX PPUSTATUS
   LDX #$3f
@@ -58,157 +73,32 @@ load_palettes:
   CPX #$20
   BNE load_palettes
 
-    ;NAMETABLES 
-    ; Ale J Pagan Andujar
-    ;STORE ALL THE A`s
-    LDA PPUSTATUS ;A
+    load_bg:
+
+    LDA PPUSTATUS
     LDA #$21
     STA PPUADDR
     LDA #$c5
     STA PPUADDR
-    LDX #$04
-    STX PPUDATA
-
-    LDA PPUSTATUS ;A
-    LDA #$21
-    STA PPUADDR
-    LDA #$cc
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;A
-    LDA #$21
-    STA PPUADDR
-    LDA #$ce
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;A
-    LDA #$21
-    STA PPUADDR
-    LDA #$d1
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;A
-    LDA #$21
-    STA PPUADDR
-    LDA #$d6
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;L
-    LDA #$21
-    STA PPUADDR
-    LDA #$c6
-    STA PPUADDR
-    LDX #$0f
-    STX PPUDATA
-
-    LDA PPUSTATUS ;E 
-    LDA #$21
-    STA PPUADDR
-    LDA #$c7
-    STA PPUADDR
-    LDX #$08
-    STX PPUDATA
-
-    LDA PPUSTATUS ;J
-    LDA #$21
-    STA PPUADDR
-    LDA #$c9
-    STA PPUADDR
-    LDX #$0D
-    STX PPUDATA
-
-    LDA PPUSTATUS ;J
-    LDA #$21
-    STA PPUADDR
-    LDA #$d5
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;P
-    LDA #$21
-    STA PPUADDR
-    LDA #$cb
-    STA PPUADDR
-    LDX #$13
-    STX PPUDATA
-
-    LDA PPUSTATUS ;G
-    LDA #$21
-    STA PPUADDR
-    LDA #$cd
-    STA PPUADDR
-    LDX #$0a
-    STX PPUDATA
     
-    ; store all the N`s
-    LDA PPUSTATUS ;N
-    LDA #$21
-    STA PPUADDR
-    LDA #$cf
-    STA PPUADDR
-    LDX #$11
-    STX PPUDATA
+    LDX #$00
+    loop:
+    LDA full_name, X
+    STA PPUDATA
+    INX
+    CPX #$15
+    BNE loop
 
-    LDA PPUSTATUS ;N
-    LDA #$21
-    STA PPUADDR
-    LDA #$d2
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ;D
-    LDA #$21
-    STA PPUADDR
-    LDA #$d3
-    STA PPUADDR
-    LDX #$07
-    STX PPUDATA
-
-    LDA PPUSTATUS ;U
-    LDA #$21
-    STA PPUADDR
-    LDA #$d4
-    STA PPUADDR
-    LDX #$18
-    STX PPUDATA
-    
-    LDA PPUSTATUS ;R
-    LDA #$21
-    STA PPUADDR
-    LDA #$d7
-    STA PPUADDR
-    LDX #$15
-    STX PPUDATA
-
-    LDA PPUSTATUS ; heart
-    LDA #$21
-    STA PPUADDR
-    LDA #$db
-    STA PPUADDR
-    LDX #$30
-    STX PPUDATA
-
-    LDA PPUSTATUS ; heart
-    LDA #$21
-    STA PPUADDR
-    LDA #$dc
-    STA PPUADDR
-    STX PPUDATA
-
-    LDA PPUSTATUS ; heart
-    LDA #$21
-    STA PPUADDR
-    LDA #$dd
-    STA PPUADDR
-    STX PPUDATA
+    ; LDA PPUSTATUS ;A
+    ; LDA #$21
+    ; STA PPUADDR
+    ; LDA #$cc
+    ; STA PPUADDR
+    ; STX PPUDATA
 
     ;ATTRIBUTE TABLES
       LDA PPUSTATUS
-      LDA #$03
+      LDA #$23
       STA PPUADDR
       LDA #$d9
       STA PPUADDR
@@ -220,14 +110,18 @@ vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
   BPL vblankwait
 
-  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+  LDA #%10000000  ; turn on NMIs, sprites use first pattern table
   STA PPUCTRL
   LDA #%00011110  ; turn on screen
   STA PPUMASK
 
 forever:
   JMP forever
-.endproc
+
+  full_name:
+  .byte $01, $02, $03, $00, $04, $01, $05, $01, $06, $00, $01, $06, $07, $08, $09, $01, $0A, $00, $00, $0b, $0b
+
+
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
@@ -250,5 +144,124 @@ sprites:
 .byte $78, $07, $00, $80
 .byte $78, $08, $00, $88
 
-.segment "CHR"
-.incbin "starfield.chr"
+
+.segment "CHARS"
+  .byte %00000000	; Empty Tile (00)
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %00000000	; A (01)
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte $3C, $7E, $C3, $C3, $FF, $FF, $C3, $C3
+
+  .byte %11000000	; L (02)
+  .byte %11000000
+  .byte %11000000
+  .byte %11000000
+  .byte %11000000
+  .byte %11000000
+  .byte %11111111
+  .byte %11111111
+  .byte $C0, $C0, $C0, $C0, $C0, $C0, $FF, $FF
+
+  .byte %11111111	; E (03)
+  .byte %11111111
+  .byte %11000000
+  .byte %11111111
+  .byte %11111111
+  .byte %11000000
+  .byte %11111111
+  .byte %11111111
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %00000000	;  P (04)
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte %00000000
+  .byte $FF, $81, $81, $BF, $A0, $A0, $A0, $E0
+
+  .byte %11111111	; G (05)
+  .byte %11111111
+  .byte %11000000
+  .byte %11001111
+  .byte %11000011
+  .byte %11000011
+  .byte %11111110
+  .byte %00111100
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %11000001	; N (06)
+  .byte %11100001
+  .byte %10110001
+  .byte %10011001
+  .byte %10001101
+  .byte %10000111
+  .byte %10000011
+  .byte %10000001
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %01111000	; D (07)
+  .byte %01000100
+  .byte %01000010
+  .byte %01000001
+  .byte %01000001
+  .byte %01000010
+  .byte %01000100
+  .byte %01111000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %01000010	; U (08)
+  .byte %01000010
+  .byte %01000010
+  .byte %01000010
+  .byte %01000010
+  .byte %01000010
+  .byte %01111110
+  .byte %00000000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %01111110	; J (09)
+  .byte %01111110
+  .byte %00011000
+  .byte %00011000
+  .byte %00011000
+  .byte %10011000
+  .byte %11111000
+  .byte %00111000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %11111110	; R (0a)
+  .byte %10000001
+  .byte %11111110
+  .byte %10100000
+  .byte %10010000
+  .byte %10001000
+  .byte %10000100
+  .byte %10000010
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
+
+  .byte %0000000	; heart (0b)
+  .byte %0011011
+  .byte %0100101
+  .byte %0100001
+  .byte %0010001
+  .byte %0001010
+  .byte %0000100
+  .byte %0000000
+  .byte $00, $00, $00, $00, $00, $00, $00, $00
